f57f4702c362c518e4da794e74be18576bc1ae96
Plague <36628963+OFWModz@users.noreply.github.com>
2021-06-07 14:29:15 +0100

    Cleanup

--- MessageBox_Version_2.cs
+++ MessageBox_Version_2.cs
@@ -25,12 +25,12 @@ namespace User32
         /// <summary>
         /// Indicates This Is A Top Most MessageBox.
         /// </summary>
-        private const uint MB_TOPMOST = 0x00040000;
+        private const uint MB_TOPMOST = 0x40000;
 
         /// <summary>
         /// Sets This MessageBox As Foreground On Creation.
         /// </summary>
-        private const uint MB_SETFOREGROUND = 0x00010000;
+        private const uint MB_SETFOREGROUND = 0x10000;
 
         /// <summary>
         /// Shows A Simple MessageBox.
@@ -170,42 +170,42 @@ namespace User32
         /// <summary>
         /// The message box contains three push buttons: Abort, Retry, and Ignore.
         /// </summary>
-        AbortRetryIgnore = 0x00000002,
+        AbortRetryIgnore = 0x2,
 
         /// <summary>
         /// The message box contains three push buttons: Cancel, Try Again, Continue.
         /// </summary>
-        CancelTryIgnore = 0x00000006,
+        CancelTryIgnore = 0x6,
 
         /// <summary>
         /// Adds a Help button to the message box.
         /// </summary>
-        Help = 0x00004000,
+        Help = 0x4000,
 
         /// <summary>
         /// The message box contains one push button: OK. This is the default.
         /// </summary>
-        Ok = 0x00000000,
+        Ok = 0,
 
         /// <summary>
         /// The message box contains two push buttons: OK and Cancel.
         /// </summary>
-        OkCancel = 0x00000001,
+        OkCancel = 0x1,
 
         /// <summary>
         /// The message box contains two push buttons: Retry and Cancel.
         /// </summary>
-        RetryCancel = 0x00000005,
+        RetryCancel = 0x5,
 
         /// <summary>
         /// The message box contains two push buttons: Yes and No.
         /// </summary>
-        YesNo = 0x00000004,
+        YesNo = 0x4,
 
         /// <summary>
         /// The message box contains three push buttons: Yes, No, and Cancel.
         /// </summary>
-        YesNoCancel = 0x00000003
+        YesNoCancel = 0x3
     }
 
     /// <summary>
@@ -216,12 +216,12 @@ namespace User32
         /// <summary>
         /// An exclamation-point icon appears in the message box.
         /// </summary>
-        Warning = 0x00000030,
+        Warning = 0x30,
 
         /// <summary>
         /// An icon consisting of a lowercase letter `i` in a circle appears in the message box.
         /// </summary>
-        Information = 0x00000040,
+        Information = 0x40,
 
         /// <summary>
         /// A question-mark icon appears in the message box.
@@ -229,12 +229,12 @@ namespace User32
         /// <remarks>
         /// The question-mark message icon is no longer recommended because it does not clearly represent a specific type of message and because the phrasing of a message as a question could apply to any message type. In addition, users can confuse the message symbol question mark with Help information. Therefore, do not use this question mark message symbol in your message boxes. The system continues to support its inclusion only for backward compatibility.
         /// </remarks>
-        Question = 0x00000020,
+        Question = 0x20,
 
         /// <summary>
         /// A stop-sign icon appears in the message box.
         /// </summary>
-        Error = 0x00000010
+        Error = 0x10
     }
 
     /// <summary>
@@ -245,22 +245,22 @@ namespace User32
         /// <summary>
         /// The first button is the default button.
         /// </summary>
-        Button1 = 0x00000000,
+        Button1 = 0,
 
         /// <summary>
         /// The second button is the default button.
         /// </summary>
-        Button2 = 0x00000100,
+        Button2 = 0x100,
 
         /// <summary>
         /// The third button is the default button.
         /// </summary>
-        Button3 = 0x00000200,
+        Button3 = 0x200,
 
         /// <summary>
         /// The fourth button is the default button.
         /// </summary>
-        Button4 = 0x00000300,
+        Button4 = 0x300,
     }
 
     /// <summary>
@@ -271,17 +271,17 @@ namespace User32
         /// <summary>
         /// The user must respond to the message box before continuing work in the window identified by the hWnd parameter. However, the user can move to the windows of other threads and work in those windows. Depending on the hierarchy of windows in the application, the user may be able to move to other windows within the thread. All child windows of the parent of the message box are automatically disabled, but pop-up windows are not.
         /// </summary>
-        Application = 0x00000000,
+        Application = 0,
 
         /// <summary>
         /// Same as <see cref="Application"/> except that the message box has the Top Most style. Use system-modal message boxes to notify the user of serious, potentially damaging errors that require immediate attention (for example, running out of memory).
         /// </summary>
-        System = 0x00001000,
+        System = 0x1000,
 
         /// <summary>
         /// Same as <see cref="Application"/> except that all the top-level windows belonging to the current thread are disabled if the hWnd parameter is NULL. Use this flag when the calling application or library does not have a window handle available but still needs to prevent input to other windows in the calling thread without suspending other threads.
         /// </summary>
-        Task = 0x00002000
+        Task = 0x2000
     }
 
     /// <summary>
